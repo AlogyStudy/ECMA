52f312afecb0d8512cf7196bea881ea8dfb68b45
alogy <xingzhanglin@gmail.com>
2017-06-19 23:10:35 +0800

    change 本地存储

---
+++ cookie.js
@@ -0,0 +1,27 @@
+var cookieRedner = (function() {
+  // 设置
+  function setValue(options) {  
+    var _default = {
+      name: null,
+      value: null,
+      expires: new Date(new Date().getTime() + (1000 * 60 * 60 * 24)),
+      path: '/',
+      domain: ''
+    };
+    for (var key in options) {
+      if (options.hasOwnPrototy(key)) {
+        _default[key] = options[key];
+      }
+    }
+    document.cookie = _default.name + '=' + escape(_default.value) + ';expires=' + _default.expires.toGMTString() + ';path=' + _default.path + ';domain=' + _default.domain;
+  }
+
+  // 获取
+  function getValue(name) {
+    var arr = document.cookie.match(new RegExp('(^| ' + name + '=([^;]*(;|$))'));
+    if (arr != null) {
+      return unescape(arr[2]);
+    }
+    return null;
+  }
+})();
